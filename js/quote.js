d640082a8878156a4cfe85dd42038a66ef982c40
Christopher Dennis <cdennis.aus@gmail.com>
2018-02-15 17:44:10 -0600

    Initial commit

---
+++ js/quote.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+function getQuote() {
+  $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
+
+    $("#quote").text('"' + data.quoteText + '"');
+    if ((data.quoteAuthor).length > 1) {
+      $("#author").text(" - " + data.quoteAuthor);
+    } else {
+      $("#author").text(" - Unknown");
+    }
+  });
+}
+
+  $("#new").on("click", function() {
+    getQuote();
+  });
+
+  $("#tweet").on("click", function() {
+    var tweetQuote = $("#quote").text() + $("#author").text();
+    window.open("https://twitter.com/intent/tweet?text=" + tweetQuote);
+  });
+
+  getQuote();
+
+});
