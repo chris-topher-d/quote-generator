e1e30e280cdcaf777b1c9eba124e6345f7073e13
Christopher Dennis <cdennis.aus@gmail.com>
2018-02-15 18:52:51 -0600

    Updated JS file

--- js/quote.js
+++ js/quote.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
 
-function getQuote() {
-  $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
-
-    $("#quote").text('"' + data.quoteText + '"');
-    if ((data.quoteAuthor).length > 1) {
-      $("#author").text(" - " + data.quoteAuthor);
-    } else {
-      $("#author").text(" - Unknown");
-    }
-  });
-}
+  function getQuote() {
+    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
+      $("#quote").text('"' + data.quoteText + '"');
+      if ((data.quoteAuthor).length > 1) {
+        $("#author").text(" - " + data.quoteAuthor);
+      } else {
+        $("#author").text(" - Unknown");
+      }
+    });
+  }
 
   $("#new").on("click", function() {
     getQuote();
@@ -20,7 +19,6 @@ function getQuote() {
     var tweetQuote = $("#quote").text() + $("#author").text();
     window.open("https://twitter.com/intent/tweet?text=" + tweetQuote);
   });
-
+  
   getQuote();
-
 });
