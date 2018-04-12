9b7f43ed628f89cde6e8f73180f35ede89e8395f
Christopher Dennis <cdennis.aus@gmail.com>
2018-04-12 18:15:04 -0500

    Added comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,34 @@
 $(document).ready(function() {
 
+  // Fetch data from API
   function getQuote() {
     $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
+
+      // Insert qoute text
       $("#quote").text('"' + data.quoteText + '"');
+
       if ((data.quoteAuthor).length > 1) {
+        // If an author is provided, prepend with a dash
         $("#author").text(" - " + data.quoteAuthor);
       } else {
+        // Else attribte quote to "Unkown"
         $("#author").text(" - Unknown");
       }
     });
   }
 
+  // Generate a new quote by calling getQuote()
   $("#new").on("click", function() {
     getQuote();
   });
 
+  // Send current quote to Twitter account
   $("#tweet").on("click", function() {
     var tweetQuote = $("#quote").text() + $("#author").text();
     window.open("https://twitter.com/intent/tweet?text=" + tweetQuote);
   });
-  
+
+  // Initial API call when page loads
   getQuote();
+
 });
